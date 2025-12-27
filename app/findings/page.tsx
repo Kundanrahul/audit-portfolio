d40f60040fa26322f47f41838ac1d06fd7f70088
Rahul Kundan <112650144+Kundanrahul@users.noreply.github.com>
2025-12-27 16:24:26 +0530

    Add revalidation to findings query fetch

--- app/findings/page.tsx
+++ app/findings/page.tsx
@@ -3,8 +3,7 @@ import { findingsQuery } from "@/sanity/lib/queries"
 import FindingsList from "./FindingsList"
 
 export default async function FindingsPage() {
-  const findings = await client.fetch(findingsQuery)
-
+  const findings = await client.fetch(findingsQuery, {}, { next: { revalidate: 60 } })
   return (
     <div className="bg-slate-900 text-slate-100 min-h-screen flex flex-col">
       <FindingsList findings={findings} />
